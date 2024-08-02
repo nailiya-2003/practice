87e0b09114386ab52018f966066e8078c675eccd
Nell <naile.gashimova@gmail.com>
2024-08-02 12:04:30 +0400

    practice-1

---
+++ script.js
@@ -0,0 +1,14 @@
+// "use strict";
+const numberOfFilms = +prompt("How much films did you look at?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+const latestFilms = prompt("What is one of your watched film?", ""),
+      evaluatedFilms = +prompt("How are you evaluate that film?", "");
+
+personalMovieDB.movies[latestFilms] = evaluatedFilms;
+console.log(personalMovieDB);
